d04023f8a2d26e638eec92c7af0159c21f13ab4d
Patrick Stahl <p.stahl@shopware.com>
2019-03-07 14:30:47 +0100

    Initial commit

---
+++ PaymentPlugin.php
@@ -0,0 +1,94 @@
+<?php declare(strict_types=1);
+
+namespace PaymentPlugin;
+
+use PaymentPlugin\Service\ExamplePayment;
+use Shopware\Core\Framework\Context;
+use Shopware\Core\Framework\DataAbstractionLayer\EntityRepositoryInterface;
+use Shopware\Core\Framework\Plugin;
+use Shopware\Core\Framework\Plugin\Context\ActivateContext;
+use Shopware\Core\Framework\Plugin\Context\DeactivateContext;
+use Shopware\Core\Framework\Plugin\Context\InstallContext;
+use Shopware\Core\Framework\Plugin\Context\UninstallContext;
+use Shopware\Core\Framework\Plugin\Helper\PluginIdProvider;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+use Symfony\Component\Config\FileLocator;
+
+class PaymentPlugin extends Plugin
+{
+    /**
+     * UUID4 for your payment method
+     */
+    public const PAYMENT_METHOD_ID = '3651742281b5496499eba1671d0e8d83';
+
+    /**
+     * The technical name of the example payment method
+     */
+    public const PAYMENT_METHOD_NAME = 'ExamplePayment';
+
+    public function build(ContainerBuilder $container): void
+    {
+        parent::build($container);
+        $loader = new XmlFileLoader($container, new FileLocator(__DIR__ . '/DependencyInjection/'));
+        $loader->load('services.xml');
+    }
+
+    public function install(InstallContext $context): void
+    {
+        $this->addPaymentMethod($context->getContext());
+    }
+
+    public function uninstall(UninstallContext $context): void
+    {
+        // Only set the payment method to inactive when uninstalling. Removing the payment method would
+        // cause data consistency issues, since the payment method might have been used in several orders
+        $this->setPaymentMethodIsActive(false, $context->getContext());
+    }
+
+    public function activate(ActivateContext $context): void
+    {
+        $this->setPaymentMethodIsActive(true, $context->getContext());
+        parent::activate($context);
+    }
+
+    public function deactivate(DeactivateContext $context): void
+    {
+        $this->setPaymentMethodIsActive(false, $context->getContext());
+        parent::deactivate($context);
+    }
+
+    private function addPaymentMethod(Context $context): void
+    {
+        /** @var PluginIdProvider $pluginIdProvider */
+        $pluginIdProvider = $this->container->get(PluginIdProvider::class);
+        $pluginId = $pluginIdProvider->getPluginIdByTechnicalName($this->getName(), $context);
+
+        $examplePaymentData = [
+            'id' => self::PAYMENT_METHOD_ID,
+            'technicalName' => self::PAYMENT_METHOD_NAME,
+            'name' => 'Example payment',
+            'additionalDescription' => 'Example payment description',
+            // Add your payment handler here
+            'class' => ExamplePayment::class,
+            'pluginId' => $pluginId,
+        ];
+
+        /** @var EntityRepositoryInterface $paymentRepository */
+        $paymentRepository = $this->container->get('payment_method.repository');
+        $paymentRepository->upsert([$examplePaymentData], $context);
+    }
+
+    private function setPaymentMethodIsActive(bool $active, Context $context): void
+    {
+        /** @var EntityRepositoryInterface $paymentRepository */
+        $paymentRepository = $this->container->get('payment_method.repository');
+
+        $paymentMethod = [
+            'id' => self::PAYMENT_METHOD_ID,
+            'active' => $active,
+        ];
+
+        $paymentRepository->update([$paymentMethod], $context);
+    }
+}
---
+++ Service/ExamplePayment.php
@@ -0,0 +1,79 @@
+<?php declare(strict_types=1);
+
+namespace PaymentPlugin\Service;
+
+use Shopware\Core\Checkout\Payment\Cart\PaymentHandler\PaymentHandlerInterface;
+use Shopware\Core\Checkout\Payment\Cart\PaymentTransactionStruct;
+use Shopware\Core\Defaults;
+use Shopware\Core\Framework\Context;
+use Shopware\Core\Framework\DataAbstractionLayer\EntityRepositoryInterface;
+use Shopware\Core\System\StateMachine\StateMachineRegistry;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+
+class ExamplePayment implements PaymentHandlerInterface
+{
+    /**
+     * @var EntityRepositoryInterface
+     */
+    private $orderTransactionRepo;
+
+    /**
+     * @var StateMachineRegistry
+     */
+    private $stateMachineRegistry;
+
+    public function __construct(EntityRepositoryInterface $orderTransactionRepo, StateMachineRegistry $stateMachineRegistry)
+    {
+        $this->orderTransactionRepo = $orderTransactionRepo;
+        $this->stateMachineRegistry = $stateMachineRegistry;
+    }
+
+    public function pay(PaymentTransactionStruct $transaction, Context $context): ?RedirectResponse
+    {
+        $stateId = $this->stateMachineRegistry->getStateByTechnicalName(Defaults::ORDER_TRANSACTION_STATE_MACHINE, Defaults::ORDER_TRANSACTION_STATES_PAID, $context)->getId();
+
+        $transactionData = [
+            'id' => $transaction->getTransactionId(),
+            'stateId' => $stateId,
+        ];
+
+        $this->orderTransactionRepo->update([$transactionData], $context);
+
+        return null;
+    }
+
+    public function finalize(string $transactionId, Request $request, Context $context): void
+    {
+        // Cancelled payment?
+        if ($request->query->getBoolean('cancel')) {
+            $stateId = $this->stateMachineRegistry->getStateByTechnicalName(Defaults::ORDER_TRANSACTION_STATE_MACHINE, Defaults::ORDER_TRANSACTION_STATES_CANCELLED, $context)->getId();
+
+            $transaction = [
+                'id' => $transactionId,
+                'stateId' => $stateId,
+            ];
+
+            $this->orderTransactionRepo->update([$transaction], $context);
+
+            return;
+        }
+
+        $paymentState = $request->query->getAlpha('status');
+
+        if ($paymentState === 'completed') {
+            // Payment completed, set transaction status to "paid"
+            $stateId = $this->stateMachineRegistry->getStateByTechnicalName(Defaults::ORDER_TRANSACTION_STATE_MACHINE, Defaults::ORDER_TRANSACTION_STATES_PAID, $context)->getId();
+        } else {
+            // Payment not completed, set transaction status to "open"
+            $stateId = $this->stateMachineRegistry->getStateByTechnicalName(Defaults::ORDER_TRANSACTION_STATE_MACHINE, Defaults::ORDER_TRANSACTION_STATES_OPEN, $context)->getId();
+        }
+
+        $transaction = [
+            'id' => $transactionId,
+            'stateId' => $stateId,
+        ];
+
+        $this->orderTransactionRepo->update([$transaction], $context);
+    }
+}
